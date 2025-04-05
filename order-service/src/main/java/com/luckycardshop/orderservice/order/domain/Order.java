fadcb32ee44103c140a1d02d3250f9016700de13
Jungmin Moon <jmmoon96@gmail.com>
2025-04-04 23:27:24 -0400

    Setting up domain entities and basic config

---
+++ order-service/src/main/java/com/luckycardshop/orderservice/order/domain/Order.java
@@ -0,0 +1,34 @@
+package com.luckycardshop.orderservice.order.domain;
+
+import java.time.Instant;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.annotation.Version;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table("orders") //order is a reserved word in sql so this annotation solves the issue when mapping
+public record Order( 
+	
+	@Id
+	Long id,
+	
+	String cardName,
+	Double cardPrice,
+	Integer quantity,
+	OrderStatus status,
+	
+	@CreatedDate
+	Instant createdDate,
+	
+	@LastModifiedDate
+	Instant lastModifiedDate,
+	
+	@Version
+	int version
+){
+	public static Order of(String cardName, Double cardPrice, Integer quantity, OrderStatus status) {
+		return new Order(null, cardName, cardPrice, quantity, status, null, null, 0);
+	}
+}
---
+++ order-service/src/main/java/com/luckycardshop/orderservice/order/domain/OrderStatus.java
@@ -0,0 +1,7 @@
+package com.luckycardshop.orderservice.order.domain;
+
+public enum OrderStatus {
+	ACCEPTED,
+	REJECTED,
+	DISPATCHED
+}
